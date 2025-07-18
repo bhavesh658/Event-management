434cfb15b85adec2338596a953402208401b0683
FrontEndproject <yadavbhavesh958@gmail.com>
2025-07-18 15:50:18 +0530

    first commit

---
+++ routes/authrouter.js
@@ -0,0 +1,11 @@
+const { signup, login } = require('../Controllers/AuthController');
+const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
+
+const router = require('express').Router();
+
+// router.post('/login', loginValidation, login);
+// router.post('/signup', (req,res)=>res.send("hiii"));
+router.post('/signup', signup);
+
+
+module.exports = router;
